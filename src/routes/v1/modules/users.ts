23393e4086e60f587ef6f85258346e2453e11753
Zaw Ye Htoo <zawyehtoo29@gmail.com>
2024-09-20 16:59:52 +0700

    added logout route

--- src/routes/v1/modules/users.ts
+++ src/routes/v1/modules/users.ts
@@ -3,10 +3,11 @@ import { userController } from "../../../controllers/v1/userController";
 import { authenticateJWT } from "../../../middleware/authenticate";
 
 const router: Router = express.Router();
-const { register, login, authMe } = userController();
+const { register, login, authMe,logout } = userController();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout",logout)
 router.get("/auth/me", authenticateJWT, authMe)
 
 export default router;
